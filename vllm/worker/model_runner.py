739af3563df7e6a1db6fdf855b6eabd9a204dbf8
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 18:22:20 +0000

    Update model_runner.py

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1937,11 +1937,6 @@ class CUDAGraphRunner(nn.Module):
         # SLIDER PROFILE #
         ##################
         self.model.model.set_slider_variables(ensure_slider_on=False)
-        if attn_metadata.seq_lens is None:
-            attn_metadata.seq_lens = attn_metadata.seq_lens_tensor.tolist()
-        print("******************")
-        print(attn_metadata.seq_lens)
-        assert 0
         with torch.cuda.graph(self._graph, pool=memory_pool, stream=stream):
             output_hidden_or_intermediate_states = self.model(
                 input_ids=input_ids,
