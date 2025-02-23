2cbf9558453ec401d63e2a9c24a5772ef30add90
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 18:21:30 +0000

    Update model_runner.py

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1917,6 +1917,8 @@ class CUDAGraphRunner(nn.Module):
             # SLIDER PROFILE #
             ##################
             self.model.model.set_slider_variables(ensure_slider_on=False)
+            if attn_metadata.seq_lens is None:
+                attn_metadata.seq_lens = attn_metadata.seq_lens_tensor.tolist()
             self.model(
                 input_ids=input_ids,
                 positions=positions,
