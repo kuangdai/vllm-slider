a8432dc1c0afa44218ed1c53f683ffcab41d39f6
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 13:49:48 +0000

    Update loader.py

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -412,11 +412,16 @@ class DefaultModelLoader(BaseModelLoader):
             # that have loaded weights tracking currently.
             if model_config.quantization is None and loaded_weights is not None:
                 weights_not_loaded = weights_to_load - loaded_weights
-                if weights_not_loaded:
+                weights_not_loaded_slider = {s for s in weights_not_loaded if "slider" in s}
+                weights_not_loaded_other = weights_not_loaded - weights_not_loaded_slider
+                if weights_not_loaded_other:
                     raise ValueError(
                         "Following weights were not initialized from "
-                        f"checkpoint: {weights_not_loaded}")
-
+                        f"checkpoint: {weights_not_loaded_other}")
+                if weights_not_loaded_slider:
+                    logger.warning("SLIDER weights were not initialized."
+                                   "This is OK. The model behaves as pretrained model, "
+                                   "and slider variables will take no effect.")
             _process_weights_after_loading(model, model_config, target_device)
 
         return model.eval()
