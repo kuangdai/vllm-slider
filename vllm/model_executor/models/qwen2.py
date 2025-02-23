02ac52c1b3c6fcf77ecbb059ca055cf7025443e7
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 16:58:30 +0000

    Update qwen2.py

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -357,17 +357,6 @@ class Qwen2DecoderLayer(nn.Module):
         return hidden_states, residual
 
 
-@support_torch_compile(
-    dynamic_arg_dims={
-        "input_ids": 0,
-        # positions is of shape (3, seq_len) if mrope is enabled for qwen2-vl,
-        # otherwise (seq_len, ).
-        "positions": -1,
-        "intermediate_tensors": 0,
-        "inputs_embeds": 0,
-    })
-
-
 ##############
 # SLIDER VAR #
 ##############
@@ -385,6 +374,15 @@ class SliderVariables:
             self.layer_copies.append(self.original.to(device=device, dtype=dtype))
 
 
+@support_torch_compile(
+    dynamic_arg_dims={
+        "input_ids": 0,
+        # positions is of shape (3, seq_len) if mrope is enabled for qwen2-vl,
+        # otherwise (seq_len, ).
+        "positions": -1,
+        "intermediate_tensors": 0,
+        "inputs_embeds": 0,
+    })
 class Qwen2Model(nn.Module):
 
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
